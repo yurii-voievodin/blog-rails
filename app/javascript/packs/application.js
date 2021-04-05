e3758086f452440c2380beaf37fefc93aa03e4d1
Yura Voevodin <voevodin.yura@icloud.com>
2021-04-05 13:57:30 +0300

    Enable tooltips and popovers - Bootstrap 5 way

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,10 +17,15 @@ import "bootstrap"
 import "../stylesheets/application"
 
 document.addEventListener("turbolinks:load", () => {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-    $('.toast').toast({ autohide: false })
-    $('#toast').toast('show')
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
 })
 
 const images = require.context('../images', true)
