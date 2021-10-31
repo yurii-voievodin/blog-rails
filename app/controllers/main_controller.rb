d5e3e91d1d0dd62813c9d9c960fa422b187bf9f8
Yura Voevodin <voevodin.yura@icloud.com>
2021-10-31 14:21:36 +0200

    Personal page - moving all data to JSON files

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class MainController < ApplicationController
 
   def main
@@ -9,6 +11,18 @@ class MainController < ApplicationController
   end
 
   def personal
+    photos_json = File.read("data/photos.json")
+    @photos = JSON.parse(photos_json)['photos']
+
+    movies_json = File.read("data/movies.json")
+    @movies = JSON.parse(movies_json)['movies']
+
+    books_json = File.read("data/books.json")
+    @books = JSON.parse(books_json)['books']
+
+    games_json = File.read("data/games.json")
+    @games = JSON.parse(games_json)['games']
+
     render "main/personal"
   end
 
--- app/controllers/ua_controller.rb
+++ app/controllers/ua_controller.rb
@@ -11,6 +11,18 @@ class UaController < ApplicationController
   end
 
   def personal
+    photos_json = File.read("data/photos.json")
+    @photos = JSON.parse(photos_json)['photos']
+
+    movies_json = File.read("data/ua/movies.json")
+    @movies = JSON.parse(movies_json)['movies']
+
+    books_json = File.read("data/ua/books.json")
+    @books = JSON.parse(books_json)['books']
+
+    games_json = File.read("data/ua/games.json")
+    @games = JSON.parse(games_json)['games']
+
     render "ua/personal"
   end
   
