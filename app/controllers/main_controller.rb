3e3dbf4f371154d46e3776bf94b11633372dcea7
Yura Voevodin <voevodin.yura@icloud.com>
2022-01-28 16:11:35 +0200

    Update role and responsibilities description

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,29 @@ require 'json'
 class MainController < ApplicationController
 
   def main
-    render "main/main"
+    skills_list = [
+      "Git",
+      "OOP",
+      "Swift",
+      "MVC",
+      "UIKit",
+      "Auto Layout",
+      "SwiftUI",
+      "Core Data",
+      "REST API",
+      "TestFlight",
+      "In-App Purchase"
+    ]
+    contacts_list = [
+      { link: "https://github.com/yura-voevodin", icon: "bi-github" },
+      { link: "https://www.linkedin.com/in/yurivoevodin", icon: "bi-linkedin" },
+      { link: "https://t.me/voevodin_yura", icon: "bi-telegram" },
+      { link: "https://www.instagram.com/voevodin.yura", icon: "bi-instagram" },
+      { link: "https://www.facebook.com/yura.voevodin", icon: "bi-facebook" },
+      { link: "https://twitter.com/voevodin_yura", icon: "bi-twitter" }
+      
+    ]
+    render "main/main", locals: { skills_list: skills_list, contacts_list: contacts_list }
   end
 
   def projects
