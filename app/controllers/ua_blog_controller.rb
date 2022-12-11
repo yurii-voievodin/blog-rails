e31b68c73629d915265e1bbade3d2d83e8561ce9
Yura Voevodin <voevodin.yura@icloud.com>
2022-12-11 14:29:42 +0200

    Add "low data mode" page

--- app/controllers/ua_blog_controller.rb
+++ app/controllers/ua_blog_controller.rb
@@ -5,5 +5,10 @@ class UaBlogController < ApplicationController
         @markdown_text = File.read("pages/ua/blog/feature.md")
         render "ua/blog/feature"
     end
+
+    def low_data_mode
+       @title = "Як оптимізувати використання мобільного Інтернет"
+        render "ua/blog/low-data-mode"
+    end
     
 end
