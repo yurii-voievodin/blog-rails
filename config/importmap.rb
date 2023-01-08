eb0b4bec54f7d78289af1210549e424dc8387717
Yura Voevodin <voevodin.yura@icloud.com>
2023-01-08 19:46:20 +0200

    Run 'rails importmap:install'

---
+++ config/importmap.rb
@@ -0,0 +1,7 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.0-beta1/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
