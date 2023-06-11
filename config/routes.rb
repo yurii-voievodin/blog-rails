9c6c1649db8bd84b87b4106459204bff2d42f09b
Yura Voevodin <voevodin.yura@icloud.com>
2023-06-11 10:55:45 +0300

    Add html to links

--- config/routes.rb
+++ config/routes.rb
@@ -7,39 +7,36 @@ Rails.application.routes.draw do
   get "/projects", to: "main#projects"
 
   # Personal (en)
-  get "/personal", to: "main#personal"
+  get "/personal", to: "main#personal", :defaults => { :format => 'html' }
 
   # Timeline (en)
-  get "/timeline", to: "main#timeline"
+  get "/timeline", to: "main#timeline", :defaults => { :format => 'html' }
 
   # UA
   scope :ua do
     # Home page
-    get "/", to: "ua#index"
-    
-    # Projects
-    get "projects", to: "ua#projects"
+    get "/", to: "ua#index", :defaults => { :format => 'html' }
 
     # Personal
-    get "personal", to: "ua#personal"
+    get "personal", to: "ua#personal", :defaults => { :format => 'html' }
 
     # Blog
-    get "blog", to: "ua#blog"
+    get "blog", to: "ua#blog", :defaults => { :format => 'html' }
 
     scope :blog do
 
       # Feature
-    get "feature", to: "ua_blog#feature"
-    get "low-data-mode", to: "ua_blog#low_data_mode"
+    get "feature", to: "ua_blog#feature", :defaults => { :format => 'html' }
+    get "low-data-mode", to: "ua_blog#low_data_mode", :defaults => { :format => 'html' }
     end
   end
 
   # Pages
   scope :pages do
     # Bootstrap 5 Timeline
-    get "bootstrap_5_timeline", to: "pages#bootstrap_timeline"
+    get "bootstrap_5_timeline", to: "pages#bootstrap_timeline", :defaults => { :format => 'html' }
 
-    get "tesla_timeline", to: "pages#tesla_timeline"
+    get "tesla_timeline", to: "pages#tesla_timeline", :defaults => { :format => 'html' }
   end
   
 end
